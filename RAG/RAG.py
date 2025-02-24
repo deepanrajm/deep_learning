42abe9d2d0f88f1a4353de6ab552c053b9e8aa3f
Deepan Raj <deepanraj.18@gmail.com>
2025-02-24 13:09:59 +0530

    rag improved

---
+++ RAG/RAG.py
@@ -0,0 +1,85 @@
+from langchain_community.document_loaders import PDFPlumberLoader
+from langchain_experimental.text_splitter import SemanticChunker
+# from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+
+from langchain.chains import RetrievalQA
+from langchain.chains.llm import LLMChain
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+from langchain.prompts import PromptTemplate
+
+llm = ChatOpenAI(openai_api_base = "http://localhost:1234/v1", openai_api_key = "lm_studio", model = "llama-3.2-1b-instruct")
+
+
+
+
+loader = PDFPlumberLoader(r"D:\Petramount\Courses\AI\LLM\1+The+structured+Prompt.pdf")
+docs = loader.load()
+
+# Check the number of pages
+# print("Number of pages in the PDF:",len(docs))
+
+# Load the random page content
+print (docs[1].page_content)
+
+text_splitter = SemanticChunker(HuggingFaceEmbeddings())
+documents = text_splitter.split_documents(docs)
+
+
+# print("Number of chunks created: ", len(documents))
+
+# print(documents[2].page_content)
+
+# Instantiate the embedding model
+embedder = HuggingFaceEmbeddings()
+
+# Create the vector store 
+vector = FAISS.from_documents(documents, embedder)
+retriever = vector.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+
+prompt = """
+1. Use the following pieces of context to answer the question at the end.
+2. If you don't know the answer, just say that "I don't know" but don't make up an answer on your own.\n
+3. Keep the answer crisp and limited to 3,4 sentences.
+
+Context: {context}
+
+Question: {question}
+
+Helpful Answer:"""
+
+
+QA_CHAIN_PROMPT = PromptTemplate.from_template(prompt) 
+
+llm_chain = LLMChain(
+                  llm=llm, 
+                  prompt=QA_CHAIN_PROMPT, 
+                  callbacks=None, 
+                  verbose=True)
+
+document_prompt = PromptTemplate(
+    input_variables=["page_content", "source"],
+    template="Context:\ncontent:{page_content}\nsource:{source}",
+)
+
+combine_documents_chain = StuffDocumentsChain(
+                  llm_chain=llm_chain,
+                  document_variable_name="context",
+                  document_prompt=document_prompt,
+                  callbacks=None,
+              )
+
+qa = RetrievalQA(
+                  combine_documents_chain=combine_documents_chain,
+                  verbose=True,
+                  retriever=retriever,
+                  return_source_documents=True,
+              )
+
+print(qa("what is target audience")["result"])
