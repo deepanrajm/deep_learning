4c50aafaca98fafdcc2baeff24ee19afe1979c75
Deepan Raj <deepanraj.18@gmail.com>
2025-05-11 15:30:14 +0530

    ollama update

--- RAG/LLM.py
+++ RAG/LLM.py
@@ -1,8 +1,14 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
+### use for lmstudio
+from langchain.chat_models import ChatOpenAI
 chat_model = ChatOpenAI(openai_api_base = "http://localhost:1234/v1", openai_api_key = "lm_studio", model = "llama-3.2-1b-instruct")
 
+###use for ollama
+# from langchain_community.llms import Ollama
+
+# llm = Ollama(model="llama3.2:1b")
 
 messages = [HumanMessage(content="Explain how 1 plus 1 is 2")]
 
